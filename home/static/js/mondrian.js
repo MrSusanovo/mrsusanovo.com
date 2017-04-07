0c40d6ac41b72e4dc27db7a605435cf67dc6fa8c
MrSusanovo <mr.susanovo@gmail.com>
2017-04-06 22:24:22 -0400

    added button to frontpage

---
+++ home/static/js/mondrian.js
@@ -0,0 +1,11 @@
+var app = angular.module('mondrian',['ngAnimate']);
+app.controller('mondCtrl', function($scope){
+    $scope.visible0 = false;
+    $scope.visible1 = false;
+    $scope.visible2 = false;
+    $scope.visible3 = false;
+    $scope.visible4 = false;
+    $scope.visible5 = false;
+    $scope.visible6 = false;
+    $scope.visible7 = false;
+});
--- home/views.py
+++ home/views.py
@@ -8,11 +8,15 @@ def index(request):
     context['cols'] = range(10)
     context['rows'] = range(5)
     context['sqrs'] = map(lambda x: {"sqr": x, "mess": ""}, range(8))
+    context['sqrs'][0]["mess"]="GitHub"
+    context['sqrs'][1]["mess"]="Click For A Brief Story About This Site"
+    context['sqrs'][2]["mess"]="My LinkedIn"
+    context['sqrs'][6]["mess"]="About Me"
     context['sqrs'][7]["mess"]="MrSusanovo"
     context['sqrs'][7]["mess1"]="Created for Dumping Junk Code."
     context['sqrs'][7]["mess2"]="(Current Page: Tribute to Mondrian.)"
     context['menu'] = ["Home","About", "Gallery","GitHub","Cool","Intro"]
-    context['scripts'] =["js/bckgrd.js","js/menu.js"]
+    context['scripts'] =["js/bckgrd.js","js/menu.js","js/mondrian.js"]
     return render(request, "index.html", context)
 
 def intro(request):
@@ -22,7 +26,7 @@ def intro(request):
     context['title'] = "Initially This Website Was Expected To Be A Personal Webpage For Co-op Job Search.It Turns Out To Be A Bad Idea Especially I Started The Project Near The End Of Term.Hosting With Apache2 Cost Me 2 Assignments And 2 Nights. This Web Page Was Accessible To The Whole World 24 Hours After I Bought The Domain.Sorry, Except For North Korea And Probably Some Country Inside The Great Fire Wall. I Spent 60 Bucks On This Server, But This SH*T Does Not Even Worth That Much. I Mean At Least It Has To Be A Dynamic Website. Otherwise The Employers Won't Be Able To Tell The Difference Between Me And Those Who Do not Know What AJAX Is. Probably They Are Hosting Their Webpages On Github And Have Higher Marks Than I Do. There Will Be More, This Is Just A Beginning. I Am Tom Feng. Welcome To MrSusanovo. Further Funcionalities Under Construction.If You See This Scentence.Probably You Are Inside The Great Fire Wall."
     context['mainPart'] = ""
     context['menu'] = ["Home","About", "Gallery","GitHub","Cool","Other"]
-    context['scripts'] =["js/title.js","js/bckgrd.js"]
+    context['scripts'] =["js/title.js","js/bckgrd.js","js/mondrian.js"]
     return render(request, "intro.html", context)
     # return HttpResponse("currently underconstruction. Problems need to be solved: 1. Django static fil(css) not loaded , got 404. 2.Django template not working.")
 
@@ -40,6 +44,10 @@ def menu_home(request):
         context['cols'] = range(10)
         context['rows'] = range(5)
         context['sqrs'] = map(lambda x: {"sqr": x, "mess": ""}, range(8))
+        context['sqrs'][0]["mess"]="GitHub"
+        context['sqrs'][1]["mess"]="Click For A Brief Story About This Site"
+        context['sqrs'][2]["mess"]="My LinkedIn"
+        context['sqrs'][6]["mess"]="About Me"
         context['sqrs'][7]["mess"]="MrSusanovo"
         context['sqrs'][7]["mess1"]="Created for Dumping Junk Code."
         context['sqrs'][7]["mess2"]="(Current Page: Tribute to Mondrian.)"
